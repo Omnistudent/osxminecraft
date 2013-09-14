0622a840ead2a36f6c5f84805ecf47b190384e3e
Omnistudent <theostenman@yahoo.com>
2013-09-14 08:54:08 +0200

    commit before branching to clienteer

---
+++ minetest.build/Release/ALL_BUILD.build/Script-1F38F30CC6FB4AF196E55F2F.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/security/osxminecraft -f /Users/security/osxminecraft/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ src/minetest.build/Release/minetest.build/Script-76F0E24A577846708FF65510.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/security/osxminecraft/src -f /Users/security/osxminecraft/src/CMakeScripts/minetest_postBuildPhase.make$CONFIGURATION all
